6b1636b4f9482d94851411920227b58b15e55fe6
asvishal <163374444+asvishal@users.noreply.github.com>
2025-11-11 15:09:59 +0530

    Change header text from 'Hey, CI/CD!' to 'A S Vishal'

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Hey, CI/CD!</h1>
+      <h1>A S Vishal</h1>
       <p>My React app is running from Jenkins!</p>
     </div>
   )
